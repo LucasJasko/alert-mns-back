42eee8845a47b358f231ba48088c691b1242b142
Lucas <lucasjaskowiak@yahoo.fr>
2025-03-29 13:21:31 +0100

    feat: ajustement du process login pour éviter les erreurs de rendu coté client

--- search.php
+++ search.php
@@ -3,16 +3,18 @@
 require_once "./include/clientAccess.php";
 require_once "./include/connect.php";
 
-$data  = json_decode(file_get_contents("php://input"), true);
-$query = $data['query'];
+$data        = json_decode(file_get_contents("php://input"), true);
+$inputSearch = $data['search'];
 
-if ($query) {
+if ($inputSearch & $inputSearch) {
     $stmt = $db->prepare("SELECT user_name FROM user WHERE user_name LIKE :search");
-    $stmt->bindValue(":search", htmlspecialchars($query));
+    $stmt->bindValue(":search", "%" . htmlspecialchars($inputSearch) . "%");
     $stmt->execute();
     if ($row = $stmt->fetchAll()) {
         echo json_encode($row);
     } else {
-        echo "Aucun résultats trouvés";
+        echo json_encode("Aucun résultats trouvés");
     }
+} else {
+    echo json_encode("");
 }
