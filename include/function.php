a079667cde71faca5e0e2cb337ae3196cea25adc
Lucas <lucasjaskowiak@yahoo.fr>
2025-04-02 13:50:40 +0200

    feat: création d'une fonction de création de fichiers logs et ajout du message de log au fichier existant

--- include/function.php
+++ include/function.php
@@ -1,6 +1,21 @@
 <?php
 function redirect($path)
 {
-  header("Location:" . $path);
-  exit();
+    header("Location:" . $path);
+    exit();
+}
+
+function writeLog($message)
+{
+    date_default_timezone_set("Europe/Paris");
+    $date = date("Y-m-d");
+    $time = date("H:i:s");
+
+    $pathfile   = "../logs/" . $date . "-logs.txt";
+    $openedfile = fopen($pathfile, "a");
+
+    $logmessage = "[ " . $date . " ] [ " . $time . " ] " . $message . "\n";
+    fwrite($openedfile, $logmessage);
+
+    fclose($openedfile);
 }
