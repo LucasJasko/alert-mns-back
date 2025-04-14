8be43c674a41aa2dc31ca21f75c40b6a5166bfcb
Lucas <lucasjaskowiak@yahoo.fr>
2025-04-14 13:40:51 +0200

    feat: ajout d'une modale de confirmation de suppression d'utilisateur

--- pages/script.js
+++ pages/script.js
@@ -1,18 +1,42 @@
 const navlink = document.querySelectorAll(".navbar li a");
 const pageName = document.querySelector(".target");
+const tableLines = document.querySelectorAll(".dashboard tbody tr");
+const deleteIcons = document.querySelectorAll(".user-btn__delete");
+const contextWindow = document.querySelector(".delete-container");
 
-switch (pageName.value) {
-  case "user":
-    navlink[1].classList.add("a-active");
-    break;
-  case "group":
-    navlink[0].classList.add("a-active");
-    break;
-  case "params":
-    navlink[2].classList.add("a-active");
-    break;
+deleteIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    contextWindow.classList.add("delete-active");
+    const userId = icon.className.replace("user-btn user-btn__delete user-btn__delete__", "");
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = `
+      <p class="delete-window__answer-text">Etes vous sûr de vouloir supprimer l'utilisateur N°${userId} ?</p>
+      <p class="delete-window__warning-text">Attention: Cette action est irréversible !</p>
+      <div class="delete-window__btn-container">
+        <button class="delete-window__btn-cancel valid-button">Annuler</button>
+        <a class="delete-window__delete-link" href="../delete.php?id=${userId}">Supprimer l'utilisateur</a>
+  </div>
+`;
+    wrapper.classList.add("delete-window");
+    contextWindow.appendChild(wrapper);
+
+    const cancelBtn = document.querySelector(".delete-window__btn-cancel");
+
+    cancelBtn.addEventListener("click", () => {
+      contextWindow.innerHTML = "";
+      contextWindow.classList.remove("delete-active");
+    });
+  });
+});
+
+// Affichage du tableau modulo
+for (let i = 0; i < tableLines.length; i++) {
+  if (i % 2 == 0) tableLines[i].style.backgroundColor = "rgb(215, 215, 215)";
 }
 
-console.log(navlink[0]);
-console.log(navlink[1]);
-console.log(navlink[2]);
+// Affichage de la navigation
+navlink.forEach((link) => {
+  if (link.innerHTML == pageName.value) link.classList.add("a-active");
+});
+
+// href="../delete.php?id=<?= $userId ?>"
--- pages/user/index.php
+++ pages/user/index.php
@@ -1,14 +1,14 @@
 <?php
 
-use core\Database;
 
 require_once $_SERVER["DOCUMENT_ROOT"] . "/class/Autoloader.php";
 Autoloader::autoload();
 
+use core\Database;
 
 $db = new Database();
-$raw = $db->selectAll("user");
-$fields = $db->getFields("user");
+$fields = $db->getFieldsOfTable("user");
+$raw = $db->getAll("user");
 
 ?>
 
@@ -19,7 +19,7 @@ $fields = $db->getFields("user");
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../index.css">
+  <link rel="stylesheet" href="../style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
   <title>Tableau de bord - Gestion des utilisateurs</title>
 </head>
@@ -39,8 +39,15 @@ $fields = $db->getFields("user");
       <li>
         <a href="../params/">Paramétrages</a>
       </li>
+      <li>
+        <a href="../stats/">Statistiques</a>
+      </li>
     </ul>
   </nav>
+
+  <div class="btn-container">
+    <a class="valid-button add-button" href="../form.php">Ajouter un utilisateur</a>
+  </div>
 
   <main class="main-container">
 
@@ -50,8 +57,8 @@ $fields = $db->getFields("user");
         <?php for ($i = 0; $i < count($fields); $i++) { ?>
           <th>
             <?php
-            $fields[$i]["Field"] = str_replace("user_", "", $fields[$i]["Field"]);
-            echo str_replace("_id", "", $fields[$i]["Field"]);
+            $fields[$i] = str_replace("user_", "", $fields[$i]);
+            echo str_replace("_id", "", $fields[$i]);
             ?>
           </th>
         <?php } ?>
@@ -60,24 +67,23 @@ $fields = $db->getFields("user");
       <tbody>
         <?php for ($i = 0; $i < count($raw); $i++) { ?>
           <tr>
-            <?php foreach ($raw[$i] as $key => $value) { ?>
+            <?php foreach ($raw[$i] as $key => $value) {
+              $userId = $raw[$i]["user_id"];
+            ?>
               <td><?= $value ?> </td>
             <?php } ?>
-            <td class="user-btn__container"> <a class="user-btn user-btn__update" href="../form.php?id=<?= $raw[$i]["user_id"] ?>"><i class="fa-solid fa-pen"></i></a> </td>
-            <td class="user-btn__container"> <a class="user-btn user-btn__delete" href=""><i class="fa-solid fa-trash-can"></i></a> </td>
+            <td class="user-btn__container"> <a class="user-btn user-btn__update" href="../form.php?id=<?= $userId ?>"><i class="fa-solid fa-pen"></i></a> </td>
+            <td class="user-btn__container"> <a class="user-btn user-btn__delete user-btn__delete__<?= $userId ?>"><i class="fa-solid fa-trash-can"></i></a> </td>
           </tr>
         <?php } ?>
       </tbody>
 
     </table>
 
-    <aside class="stats">
-      <h2>Statistiques</h2>
-    </aside>
-
+    <div class="delete-container"></div>
   </main>
 
-  <input type="text" class="target" value="user" hidden>
+  <input type="text" class="target" value="Utilisateurs" hidden>
   <script src="../script.js"></script>
 
 </body>
