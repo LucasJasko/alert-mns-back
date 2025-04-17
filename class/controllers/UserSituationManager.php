1a05a137915cad4e7f30f814e82d5ca8f7027bbe
Lucas <lucasjaskowiak@yahoo.fr>
2025-04-17 11:31:48 +0200

    feat: modularisation des manager de petites tables (paramétrages) et réutilisation pour la création du manager de département

--- class/controllers/UserSituationManager.php
+++ class/controllers/UserSituationManager.php
@@ -8,40 +8,42 @@ use core\Log;
 class UserSituationManager
 {
 
+  private $tableName;
   private $db;
   private $data;
 
   public function __construct()
   {
     $this->db = new Database();
+    $this->tableName = "user_situation";
   }
 
   public function createUserSituation(array $data)
   {
-    $fields =  $this->db->getFieldsOfTable("user_situation");
+    $fields =  $this->db->getFieldsOfTable($this->tableName);
     for ($i = 0; $i < count($fields); $i++) {
       if (!isset($data[$fields[$i]])) $data[$fields[$i]] = "";
     }
-    $this->db->createOne("user_situation", $fields, $data);
+    $this->db->createOne($this->tableName, $fields, $data);
     Log::writeLog("Une situation a été ajouté à la base de donnée.");
   }
 
   public function getUserSituation(int $id)
   {
-    $row = $this->db->getAllWhere("user_situation", "user_id", $id);
+    $row = $this->db->getAllWhere($this->tableName, $this->tableName . "_id", $id);
     return $row;
   }
 
   public function updateUserSituation(int $id, array $newData)
   {
-    $param = "user_id";
+    $param = $this->tableName . "_id";
     $this->data = $newData;
-    $res = $this->db->updateOne("user_situation", $this->data, $param, $id);
+    $res = $this->db->updateOne($this->tableName, $this->data, $param, $id);
   }
 
   public function deleteUserSituation(int $id)
   {
-    $this->db->deleteOne("user_situation", "user_id", $id);
+    $this->db->deleteOne($this->tableName, $this->tableName . "_id", $id);
     Log::writeLog("La situation " . $id . " a été supprimé de la base de donnée.");
   }
 }
