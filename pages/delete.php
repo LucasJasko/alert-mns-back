269b84652909752f4831c57b09f6c7a36e5deb01
Lucas <lucasjaskowiak@yahoo.fr>
2025-04-17 11:38:15 +0200

    feat: ajout de l'attribut class_name dans le process de suppression

--- pages/delete.php
+++ pages/delete.php
@@ -4,10 +4,10 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/class/Autoloader.php";
 Autoloader::autoload();
 
 
-if (isset($_GET["form_type"]) && isset($_GET["id"])) {
-  $managerName = "controllers\\" . ucfirst($_GET["form_type"]) . "Manager";
+if (isset($_GET["form_type"]) && isset($_GET["id"]) && isset($_GET["class_name"])) {
+  $managerName = "controllers\\" . $_GET["class_name"] . "Manager";
   $manager = new $managerName();
-  $method = "delete" . ucfirst($_GET["form_type"]);
+  $method = "delete" . $_GET["class_name"];
   $manager->$method($_GET["id"]);
   header("Location:/pages/" . $_GET["form_type"] . "/index.php");
 }
