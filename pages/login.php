3280bfff8ac6743df4bff39094724f294349af06
Lucas <lucasjaskowiak@yahoo.fr>
2025-05-13 09:40:24 +0200

    style: mise en page de la page login accès au portail

--- pages/login.php
+++ pages/login.php
@@ -1,8 +1,4 @@
-<?php
-
-$err = null;
-
-?>
+<?php $err = null; ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,18 +6,22 @@ $err = null;
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
   <title>Portail d'accès admin</title>
 </head>
 
 <body>
-  <h1>Back office du projet alert-mns</h1>
+  <h1>Alert-MNS: Portail d'accès admin</h1>
 
-  <form action="index.php" method="post">
-    <input type="text" name="email" placeholder="email">
-    <input type="text" name="password" placeholder="password">
-    <input type="submit" value="Envoyer"> <br>
-    <?php if (!is_null($err)) echo $err; ?>
-  </form>
+  <div class="login-window">
+    <form class="login-form form" action="index.php" method="post">
+      <input type="text" name="email" placeholder="email">
+      <input type="password" name="password" placeholder="password">
+      <input class="valid-button" type="submit" value="Connexion"> <br>
+      <?php if (!is_null($err)) echo $err; ?>
+    </form>
+  </div>
 
 </body>
 
