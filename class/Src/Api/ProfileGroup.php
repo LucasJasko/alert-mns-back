f864ec206e7cc1ce7349898645c91fff4399ddef
Lucas Jaskowiak <lucasjaskowiak@yahoo.fr>
2025-06-04 07:07:38 +0200

    feat: modification de la réponse à la récupération des informations des groupes d'un utilisateur

--- class/Src/Api/ProfileGroup.php
+++ class/Src/Api/ProfileGroup.php
@@ -16,6 +16,15 @@ class ProfileGroup
         $res[] = $db->getMultipleWhere("group", ["group_id", "group_name", "group_picture"], "group_id", $relations[$i]["group_id"]);
       }
 
+      for ($i = 0; $i < count($res); $i++) {
+        $res[$i]["id"] = $res[$i]["group_id"];
+        $res[$i]["name"] = $res[$i]["group_name"];
+        $res[$i]["picture"] = $res[$i]["group_picture"];
+        unset($res[$i]["group_id"]);
+        unset($res[$i]["group_name"]);
+        unset($res[$i]["group_picture"]);
+      }
+
       echo json_encode($res);
 
       return;
